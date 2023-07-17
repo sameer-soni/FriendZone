00c430ebb72e4dff269066b0dff15a6b5b8f78fc
MMW <null.mmw@gmail.com>
2023-07-17 23:22:12 +0500

    Code Cleaned

---
+++ frontend/src/routes/routes.jsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "./Layout";
+import { Home, SignIn, SignUp } from "../pages";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+  {
+    path: "/signUp/",
+    element: <SignUp />,
+  },
+  {
+    path: "/signIn",
+    element: <SignIn />,
+  },
+]);
+
+export default router;
