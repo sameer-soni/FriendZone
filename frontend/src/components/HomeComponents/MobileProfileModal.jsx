6a4cf93593ae74ac5b88600a3b49e5c1e162864e
MMW <null.mmw@gmail.com>
2023-07-22 08:45:16 +0500

    Git Bug Fixed

--- frontend/src/components/HomeComponents/MobileProfileModal.jsx
+++ frontend/src/components/HomeComponents/MobileProfileModal.jsx
@@ -1,11 +1,9 @@
-import { Fragment, useContext } from "react";
+import { Fragment } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import PropTypes from "prop-types";
-import Button from "../ReUseableComponents/Button";
-import { MyContext } from "../../context/MyContext";
+import { XMarkIcon } from "@heroicons/react/24/outline";
 
-const MobileProfileModal = ({ open, setOpen, handleLogout }) => {
-  const { loggedUser } = useContext(MyContext);
+const MobileProfileModal = ({ open, setOpen, user, children }) => {
   return (
     <Transition.Root show={open} as={Fragment}>
       <Dialog as="div" className="relative z-20" onClose={setOpen}>
@@ -33,17 +31,30 @@ const MobileProfileModal = ({ open, setOpen, handleLogout }) => {
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
               <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white   pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm ">
-                <div className="flex flex-col">
+                <div className="flex flex-col relative ">
+                  <div className="flex h-7 items-center absolute right-5 top-2">
+                    <button
+                      type="button"
+                      className="rounded-md bg-white text-primary-shade "
+                      onClick={() => setOpen(false)}
+                    >
+                      <span className="sr-only">Close panel</span>
+                      <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                    </button>
+                  </div>
                   <div>
                     <img src="https://images.unsplash.com/photo-1542401886-65d6c61db217?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" />
                   </div>
                   <div>
                     <div className="relative flex flex-col items-center justify-center ">
                       <div className="absolute -top-20 w-36 h-36 border-8 border-white drop-shadow-2xl rounded-full overflow-hidden ">
-                        <img src={loggedUser?.pic} />
+                        <img
+                          src={user?.pic}
+                          className="w-full h-full object-cover object-left"
+                        />
                       </div>
                       <div className="mt-20 text-dark-shade text-1xl font-bold">
-                        {loggedUser?.username}
+                        {user?.username}
                       </div>
                       <div className="flex flex-row items-center justify-between w-full mt-8 px-8">
                         <div className="flex flex-col items-start justify-center">
@@ -51,7 +62,7 @@ const MobileProfileModal = ({ open, setOpen, handleLogout }) => {
                             Email
                           </div>
                           <div className="text-dark-shade font-semibold text-sm">
-                            {loggedUser?.email}
+                            {user?.email}
                           </div>
                         </div>
                         <div className="flex flex-col items-start justify-center">
@@ -75,13 +86,8 @@ const MobileProfileModal = ({ open, setOpen, handleLogout }) => {
                           software products.
                         </div>
                       </div>
-                      <div className="flex flex-row items-center justify-center w-full  px-8 mt-8">
-                        <Button
-                          clickHandler={handleLogout}
-                          className="bg-primary-shade hover:bg-primary-shade-v2 text-white focus:outline-none"
-                        >
-                          Log Out
-                        </Button>
+                      <div className="flex flex-row items-center justify-around w-full  px-8 mt-8">
+                        {children}
                       </div>
                     </div>
                   </div>
@@ -98,7 +104,9 @@ const MobileProfileModal = ({ open, setOpen, handleLogout }) => {
 MobileProfileModal.propTypes = {
   open: PropTypes.bool.isRequired,
   setOpen: PropTypes.func.isRequired,
-  handleLogout: PropTypes.func.isRequired,
+  handleLogout: PropTypes.func,
+  user: PropTypes.object,
+  children: PropTypes.node,
 };
 
 export default MobileProfileModal;
