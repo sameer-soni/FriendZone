c2d7fcf3981608c43e9a0b6b80fbc89b1dab01ec
MMW <null.mmw@gmail.com>
2023-07-24 03:57:23 +0500

    Sidebar UX improved

--- frontend/src/components/ReUseableComponents/Sidebar.jsx
+++ frontend/src/components/ReUseableComponents/Sidebar.jsx
@@ -1,9 +1,10 @@
-import { classNames } from "../../utils/Helpers";
 import { navigation } from "../../constants/Constants";
 import UserProfileCard from "./UserProfileCard";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Sidebar = () => {
+  const [currentPage, setCurrentPage] = useState("");
   return (
     // Sidebar container with styles for width, background color, and other properties.
     <div className="bg-main-shade hidden w-64 overflow-y-auto md:block overflow-x-hidden  drop-shadow-2xl">
@@ -17,16 +18,23 @@ const Sidebar = () => {
                 {/* Navigation links */}
                 {navigation.map((item) => (
                   <Link
+                    onClick={() => setCurrentPage(item.name)}
                     key={item.name}
                     to={item.href}
                     // Dynamic class names based on link properties and hover effects.
-                    className={classNames(
-                      "text-text-color border-l-4 border-main-shade hover:border-primary-shade hover:text-primary-shade group flex items-center py-2 text-sm font-medium transition duration-500 ease-in-out px-5"
-                    )}
+                    className={` border-l-4 border-main-shade hover:border-primary-shade hover:text-primary-shade group flex items-center py-2 text-sm font-medium transition duration-500 ease-in-out px-5 ${
+                      currentPage == item.name
+                        ? "border-primary-shade text-primary-shade"
+                        : "text-text-color"
+                    } `}
                   >
                     {/* Icon for the link */}
                     <item.icon
-                      className="text-text-color group-hover:text-primary-shade transition duration-500 ease-in-out mr-3 h-6 w-6"
+                      className={`group-hover:text-primary-shade transition duration-500 ease-in-out mr-3 h-6 w-6  ${
+                        currentPage == item.name
+                          ? "text-primary-shade"
+                          : "text-text-color"
+                      } `}
                       aria-hidden="true"
                     />
                     {/* Link name */}
