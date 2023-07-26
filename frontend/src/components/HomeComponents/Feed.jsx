995406aa9f0f5edcae038110c845d98355058adc
MMW <null.mmw@gmail.com>
2023-07-27 03:17:39 +0500

    Comments Bug Fixed

--- frontend/src/components/HomeComponents/Feed.jsx
+++ frontend/src/components/HomeComponents/Feed.jsx
@@ -2,7 +2,7 @@
 import PropTypes from "prop-types";
 import { useContext, useEffect, useState } from "react";
 import { MyContext } from "../../context/MyContext";
-import { ActivityIcon } from "../index";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 import { BsThreeDots, BsShare } from "react-icons/bs";
 import {
   AiOutlineLike,
@@ -11,16 +11,16 @@ import {
   AiOutlineSend,
 } from "react-icons/ai";
 import { Button } from "../index";
-import { generateRandomFeedComments } from "../../constants/Constants";
 import axios from "axios";
 
 // Component definition for Feed
-const Feed = ({ feed, contentImg }) => {
+const Feed = ({ feed }) => {
+  const [openComments, setOpenComments] = useState(false);
+
   // Using the useContext hook to get data from MyContext
   const { loggedUser } = useContext(MyContext);
 
   // Generating random feed comments using a utility function
-  const randomFeedComments = generateRandomFeedComments();
 
   const [comment, setComment] = useState("");
 
@@ -161,7 +161,25 @@ const Feed = ({ feed, contentImg }) => {
               </div>
             </div>
           </div>
-          <div className="flex flex-col w-full px-3 my-5 ">
+          <div className="flex flex-row items-center justify-between px-2 my-5 ">
+            <div
+              onClick={() => setOpenComments(!openComments)}
+              className="flex flex-row items-center font-bold  duration-500 cursor-pointer"
+            >
+              View Comments
+              {openComments ? (
+                <IoIosArrowUp className="ml-1" />
+              ) : (
+                <IoIosArrowDown className="ml-1" />
+              )}
+            </div>
+          </div>
+
+          <div
+            className={`${
+              openComments ? "flex" : "hidden"
+            }  flex-col w-full px-3 my-5`}
+          >
             {/* Rendering random feed comments */}
             {postComments?.map((comment) => (
               <div key={comment._id} className="my-5">
@@ -174,109 +192,10 @@ const Feed = ({ feed, contentImg }) => {
                     <div className="font-bold text-sm">
                       {comment?.postedBy?.username}
                     </div>
-                    <div className="flex flex-col">
-                      {/* <span className="flex flex-row items-center justify-start space-x-1">
-                        <p className="text-xs font-medium text-white"></p>
-                      </span> */}
-                    </div>
                   </div>
                 </div>
-                <div className="w-full">{comment?.text}</div>
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className="flex flex-row w-full items-center justify-between px-2 pt-4">
-          {/* Like button */}
-          <span
-            onClick={() => console.log("image liked")}
-            className="flex flex-row items-center justify-center"
-          >
-            <AiOutlineLike className="w-7 h-7" />
-            <p className="font-bold ml-1">Like</p>
-          </span>
-          {/* Dislike button */}
-          <span
-            onClick={() => console.log("image disliked")}
-            className="flex flex-row items-center justify-center"
-          >
-            <AiOutlineDislike className="w-7 h-7" />
-            <p className="font-bold ml-1">Dislike</p>
-          </span>
-          {/* Share button */}
-          <span
-            onClick={() => console.log("image share")}
-            className="flex flex-row items-center justify-center"
-          >
-            <BsShare className="w-5 h-5" />
-            <p className="font-bold ml-1">Share</p>
-          </span>
-        </div>
-        <div className="w-full">
-          <div className="flex flex-row items-center px-2 w-full pt-5">
-            <img
-              className="inline-block w-9 rounded-full object-cover h-9"
-              src={loggedUser?.pic}
-              alt=""
-            />
-            <div className="flex flex-1 items-center justify-center px-2 w-full">
-              <div className="w-full">
-                <div className="relative w-full">
-                  <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                    <AiOutlineComment
-                      className="h-5 w-5 text-gray-400"
-                      aria-hidden="true"
-                    />
-                  </div>
-                  {/* Comment input */}
-                  <input
-                    id="search"
-                    name="search"
-                    className="block w-full rounded-md font-bold border border-transparent bg-seconday-shade text-text-color py-2 pl-10 pr-3 text-sm sm:text-sm"
-                    placeholder={`Comment...`}
-                    type="search"
-                    autoComplete="off"
-                  />
-                  <div className="absolute inset-y-0 right-0 flex items-center pr-3">
-                    {/* Send button */}
-                    <Button
-                      type="button"
-                      className="inline-flex items-center rounded-full border border-transparent bg-primary-shade text-white shadow-sm focus:outline-none focus:ring-2 border-white p-1"
-                    >
-                      <AiOutlineSend className="h-4 w-4" aria-hidden="true" />
-                    </Button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="flex flex-col w-full px-2 my-5">
-            {/* Rendering random feed comments */}
-            {randomFeedComments.map((comment) => (
-              <div key={comment.user.id} className="my-5">
-                <div className="flex flex-row items-center justify-start my-2 ">
-                  <img
-                    className="inline-block w-9 rounded-full object-cover h-9"
-                    src={comment.user.pic}
-                  />
-                  <div className="flex flex-col items-start justify-start ml-1">
-                    <div className="font-bold text-sm">
-                      {comment.user.username}
-                    </div>
-                    <div className="flex flex-col">
-                      <span className="flex flex-row items-center justify-start space-x-1">
-                        {/* Activity icon based on user status */}
-                        <ActivityIcon status={comment.user.status} />
-                        <p className="text-xs font-medium text-white">
-                          {comment.user.status}
-                        </p>
-                      </span>
-                    </div>
-                  </div>
-                </div>
-                <div className="w-full">
-                  {/* Text content of the comment */}
-                  {comment.comment}
+                <div className="w-full bg-seconday-shade py-2 pl-5 rounded-md">
+                  {comment?.text}
                 </div>
               </div>
             ))}
